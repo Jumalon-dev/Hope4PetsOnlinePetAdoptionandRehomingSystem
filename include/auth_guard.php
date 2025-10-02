f3da8145a8b8aaa24f629c239eef0aac4b97488f
SaiBelarmino <sairabelarmino1@gmail.com>
2025-10-02 06:37:31 +0000

    include

---
+++ include/auth_guard.php
@@ -0,0 +1,9 @@
+<?php
+// Simple session guard to protect authenticated pages
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+if (!isset($_SESSION['user_id'])) {
+    header('Location: /Hope4PetsOnlinePetAdoptionandRehomingSystem/pages_2/authentication-login.php');
+    exit;
+}
